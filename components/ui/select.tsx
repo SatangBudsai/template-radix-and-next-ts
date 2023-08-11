db4aec3ba253c0a2f3b315049d73aa69c1063d25
satang_budsai <satang_budsai@cmu.ac.th>
2023-08-11 14:47:51 +0700

    update canlendar

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -19,14 +19,14 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      "flex h-10 w-full items-center justify-between rounded-lg border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
       className
     )}
     {...props}
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <Icon icon="lucide:chevron-down" className="h-4 w-4 opacity-50" />
+      <Icon icon="lucide:chevron-down" className="h-4 w-4 opacity-50 ml-2" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ))
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 import MainLayout from '@/components/layouts/MainLayout'
 import RootLayout from '@/components/layouts/RootLayout'
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
 import { useRouter } from 'next/router'
 import React, { ReactElement } from 'react'
-
 type Props = {}
 
 const Home = (props: Props) => {
   const router = useRouter()
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+
   return (
     <div className='text-4xl font-bold p-5 flex items-center gap-2'>
       Home Page
       <Button onClick={() => router.push("/dashboard")} > Dashboard</Button>
+      <Calendar
+        mode="single"
+        captionLayout='dropdown-buttons'
+        fromYear={2000}
+        toYear={2023}
+        selected={date}
+        onSelect={setDate}
+      />
     </div >
   )
 }
