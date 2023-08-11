772bbd312c70508b0a1a2a88fa577236470fde0d
satang_budsai <satang_budsai@cmu.ac.th>
2023-08-11 17:16:10 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,12 @@ import RootLayout from '@/components/layouts/RootLayout'
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
 import { DatePicker } from '@/components/ui/date-picker'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { useRouter } from 'next/router'
 import React, { ReactElement } from 'react'
+import { Icon } from '@iconify/react';
+import { format } from 'date-fns';
+import { cn } from '@/lib/utils'
 type Props = {}
 
 const Home = (props: Props) => {
@@ -14,8 +18,30 @@ const Home = (props: Props) => {
   return (
     <div className='flex flex-col gap-5'>
       <Button onClick={() => router.push("/dashboard")} > Dashboard</Button>
-      <DatePicker toYear={2025} />
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn("w-[240px] justify-start text-left font-normal", !date && "text-muted-foreground")}
+          >
+            <Icon icon="solar:calendar-outline" className="mr-2 h-5 w-5" />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent align="start" className="w-auto p-0 rounded-xl">
+          <Calendar
+            mode="single"
+            captionLayout="dropdown-buttons"
+            defaultMonth={date}
+            selected={date}
+            onSelect={setDate}
+            fromYear={1960}
+            toYear={2030}
+          />
+        </PopoverContent>
+      </Popover>
       <Calendar captionLayout='dropdown-buttons' className='bg-card border rounded-xl w-fit' />
+
     </div >
   )
 }
