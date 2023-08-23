da50b1a0fba4623bfd7977077ac60b91eb1bd53b
satang_budsai <satang_budsai@cmu.ac.th>
2023-08-23 17:08:10 +0700

    update

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -1,11 +1,90 @@
-import React from 'react'
+"use client"
 
-type Props = {}
+import * as React from "react"
 
-const combobox = (props: Props) => {
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Icon } from '@iconify/react';
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+} from "@/components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+const frameworks = [
+    {
+        value: "next.js",
+        label: "Next.js",
+    },
+    {
+        value: "sveltekit",
+        label: "SvelteKit",
+    },
+    {
+        value: "nuxt.js",
+        label: "Nuxt.js",
+    },
+    {
+        value: "remix",
+        label: "Remix",
+    },
+    {
+        value: "astro",
+        label: "Astro",
+    },
+]
+
+const Combobox = () => {
+    const [open, setOpen] = React.useState(false)
+    const [value, setValue] = React.useState("")
+
     return (
-        <div>combobox</div>
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between"
+                >
+                    {value
+                        ? frameworks.find((framework) => framework.value === value)?.label
+                        : "Select framework..."}
+                    <Icon icon="mingcute:down-fill" className="ml-2 h-4 w-4 shrink-0 text-muted-foreground" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandInput placeholder="Search framework..." />
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                        {frameworks.map((framework) => (
+                            <CommandItem
+                                key={framework.value}
+                                onSelect={(currentValue) => {
+                                    setValue(currentValue === value ? "" : currentValue)
+                                    setOpen(false)
+                                }}
+                            >
+                                <Icon icon="iconamoon:check-bold" className={cn(
+                                    "mr-2 h-4 w-4",
+                                    value === framework.value ? "opacity-100" : "opacity-0"
+                                )} />
+                                {framework.label}
+                            </CommandItem>
+                        ))}
+                    </CommandGroup>
+                </Command>
+            </PopoverContent>
+        </Popover>
     )
 }
 
-export default combobox
+export default Combobox
