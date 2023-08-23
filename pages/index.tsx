4d90af7f999312582ad896106b2f5bc74347116a
satang_budsai <satang_budsai@cmu.ac.th>
2023-08-23 17:01:30 +0700

    update delete componetnt InputDate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,13 @@
 import MainLayout from '@/components/layouts/MainLayout'
 import RootLayout from '@/components/layouts/RootLayout'
 import { Button } from '@/components/ui/button'
-import { Calendar, InputDate } from '@/components/ui/calendar'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Calendar } from '@/components/ui/calendar'
 import { useRouter } from 'next/router'
 import React, { ReactElement, useState } from 'react'
-import { Icon } from '@iconify/react';
-import { cn } from '@/lib/utils'
-import dayjs from 'dayjs'
-import { DateFormat } from '@/utils/date-format'
 import { DateRange } from 'react-day-picker'
-import { DatePicker } from '@/components/ui/date-picker'
-import { DateMultiplePicker } from '@/components/ui/date-multiple-picker'
-import { DateRangePicker } from '@/components/ui/date-range-picker'
-
+import DatePicker from '@/components/ui/date-picker'
+import DateMultiplePicker from '@/components/ui/date-multiple-picker'
+import DateRangePicker from '@/components/ui/date-range-picker'
 type Props = {}
 
 const Home = (props: Props) => {
@@ -26,34 +20,6 @@ const Home = (props: Props) => {
     <div className='flex flex-col gap-5'>
       <Button onClick={() => router.push("/dashboard")} >Dashboard</Button>
       <Calendar captionLayout='buttons' className='bg-card border rounded-xl w-fit' />
-      <InputDate placeholder='Pick a date' selected={date}>
-        <Calendar
-          mode="single"
-          captionLayout='dropdown-buttons'
-          selected={date}
-          onSelect={setDate}
-          defaultMonth={date}
-        />
-      </InputDate>
-      <InputDate placeholder='Pick a date' selected={arrDate}>
-        <Calendar
-          mode="multiple"
-          captionLayout='dropdown-buttons'
-          min={1}
-          selected={arrDate}
-          onSelect={setArrDate}
-        />
-      </InputDate>
-      <InputDate placeholder='Pick a date' selected={rangeDate}>
-        <Calendar
-          mode="range"
-          defaultMonth={rangeDate?.from}
-          captionLayout='dropdown-buttons'
-          selected={rangeDate}
-          onSelect={setRangeDate}
-          numberOfMonths={2}
-        />
-      </InputDate>
       <DatePicker
         mode="single"
         placeholder='Picker Date'
